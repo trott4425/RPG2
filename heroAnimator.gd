03c0dd047c2676383b2deb67b53ac2077fa50396
James trott <trott4425@gmail.com>
2017-09-25 22:33:49 -0400

    no message

--- heroAnimator.gd
+++ heroAnimator.gd
@@ -9,7 +9,7 @@ func _process(delta):
    tempElapsed = tempElapsed + delta
    
    if(tempElapsed > 0.1):
-      if(get_frame() == self.get_sprite_frames().get_frame_count('default')-1):
+      if(get_frame() == self.get_sprite_frames().get_frame_count('left_walking')-1):
          set_frame(0)
       else:
          self.set_frame(get_frame() + 1)
